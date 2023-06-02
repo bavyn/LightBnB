368ee8dad0a4542ddca0ba3434dd279e981e37ab
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-01 22:19:06 -0700

    refactor getUserWithEmail, getUserWithId, addUser to use retrieve from database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -19,24 +19,47 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
+const getUserWithEmail = (email) => {
+  return pool
+  .query(`
+    SELECT id, name, email, password
+    FROM users
+    WHERE email = $1
+  `, [email])
+  .then((res) => {
+    if(!res.rows.length) {
+      return(null)
     }
-  }
-  return Promise.resolve(resolvedUser);
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
+
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = (id) => {
+  return pool
+  .query(`
+    SELECT id, name, email, password
+    FROM users
+    WHERE id = $1
+  `, [id])
+  .then((res) => {
+    if(!res.rows.length) {
+      return(null)
+    }
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /**
@@ -44,12 +67,22 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-};
+const addUser = (user) => {
+  return pool
+  .query(`INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *`,
+    [user.name, user.email, user.password])
+  .then((res) => {
+    if(!res.rows.length) {
+      return(null)
+    }
+    return res.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+}
 
 /// Reservations
 
